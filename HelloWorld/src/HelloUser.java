e087c7243c908d7c6e37df5e2f90316c34f76a6b
Liachtei <c.lichtmannegger@yahoo.de>
2015-10-19 13:59:19 +0200

    File changed
    File Changed

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class HelloUser {
+
+	String userName;
+	
+	void greetUser()
+	{
+		System.out.println("Hello ASE2015 - how are you? :)\nPlease enter your name: Christian");
+	}
+}
